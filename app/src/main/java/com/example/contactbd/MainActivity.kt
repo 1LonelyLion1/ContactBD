118b725cd1f612d996f609b2491c17bdc295fc74
LonelyLion <patlaysemen@gmail.com>
2020-11-16 17:06:42 +0700

    add request permissions

--- app/src/main/java/com/example/contactbd/MainActivity.kt
+++ app/src/main/java/com/example/contactbd/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.contactbd
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
@@ -7,16 +9,16 @@ import android.os.Handler
 import android.os.Looper
 import android.provider.ContactsContract
 import android.text.method.ScrollingMovementMethod
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.contactbd.databas.Contact
 import com.example.contactbd.databas.ContactDao
 import com.example.contactbd.databas.ContactDataBase
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,13 +27,45 @@ class MainActivity : AppCompatActivity() {
     private var contactDAO: ContactDao? = null
 
 
+
     private var contacts: MutableList<Contact> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tvTextView: TextView = findViewById(R.id.tv_text)
 
+        requestPermissionIfNecessary()
+    }
+
+    private fun isPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            arrayOf(Manifest.permission.READ_CONTACTS).toString()
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermissionIfNecessary() {
+        if (!isPermissionsGranted()) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), 42)
+        }
+        else{
+            loadAndShowBD()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+           42 -> loadAndShowBD()
+           else -> tv_text.text = getString(R.string.messagePermissionsDenied)
+        }
+
+    }
+
+
+    private fun loadAndShowBD(){
         Observable.fromCallable {
 
             db = ContactDataBase.getContactDataBase(context = this) //Почемуто крашит приложуху((
@@ -40,22 +74,23 @@ class MainActivity : AppCompatActivity() {
 
             val uri: Uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
             val cursor: Cursor? = contentResolver
-                ?.query(uri, null, null, null, null)
+                    ?.query(uri, null, null, null, null)
 
             var id = 0
 
             if (cursor != null) {
                 while (cursor.moveToNext()) {
                     val name: String =
-                        cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
                     val phone =
-                        cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
 
-                    contacts.add(Contact( id,name, phone))
+                    contacts.add(Contact(id, name, phone))
 
                     id++
                 }
             }
+
             cursor?.close()
 
             contactDAO?.insertContact(contacts)
@@ -63,27 +98,23 @@ class MainActivity : AppCompatActivity() {
 
             db?.contactDAO()?.getContacts()
 
-        }.doOnNext {
-                MutableList ->
+        }.doOnNext { MutableList ->
             var finalString = ""
             MutableList?.map { finalString += it.name + " " + it.phone + "\n" }
 
-            val handler = Handler(Looper.getMainLooper())
 
-            handler.post {
-                run {
+            Handler(Looper.getMainLooper()).post {
+                 run {
 
-                    tvTextView.text = finalString
-                    tvTextView.movementMethod = ScrollingMovementMethod()
+                    tv_text.text = finalString
+                    tv_text.movementMethod = ScrollingMovementMethod()
                 }
-            }
-
-
+             }
 
         }.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe()
-
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
 
     }
+
 }
